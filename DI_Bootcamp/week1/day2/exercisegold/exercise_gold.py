ba0e46af94b14b739baebf930c03c419d429aa2b
naoboco <naomiebocobza@gmail.com>
2026-07-13 19:43:56 +0000

    Complete day 2 gold exercise

--- DI_Bootcamp/week1/day2/exercisegold/exercise_gold.py
+++ DI_Bootcamp/week1/day2/exercisegold/exercise_gold.py
@@ -1,1 +1,15 @@
 print(("Hello world\n" * 4) + ("I love python\n" * 4))
+
+
+month = int(input("Enter a month number from 1 to 12: "))
+
+if month >= 3 and month <= 5:
+    print("Spring")
+elif month >= 6 and month <= 8:
+    print("Summer")
+elif month >= 9 and month <= 11:
+    print("Autumn")
+elif month == 12 or month == 1 or month == 2:
+    print("Winter")
+else:
+    print("Invalid month")
