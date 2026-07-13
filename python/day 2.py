479040ea83d30b8f0eaf2325382f1ba60fc10453
naoboco <naomiebocobza@gmail.com>
2026-07-13 18:44:28 +0000

    Complete Day 2 exercises

---
+++ python/day 2.py
@@ -0,0 +1,38 @@
+#exercice 4
+
+computer_brand  = "Lenovo"
+
+print(f"I have a {computer_brand} computer")
+
+#exercie5
+
+name = "Naomie"
+age = 25
+shoe_size = 39
+
+info = f"my name is {name}, I am {age}years old, my shoe size is {shoe_size}."
+
+print (info)
+
+#exercice 6
+
+a = 10
+b = 5 
+
+if a > b:
+    print ("hello world")
+
+#exercice 8
+my_name ="Naomie"
+if user_name == my_name:
+    print("only one of us can be called this name prepare to die")
+else:
+    ("nice to meet you my name is naomie the only one")
+
+#exercice 9 
+height = input("Enter your height: ")
+
+if int(height) > 145:
+    print("You are Tall enough to ride")
+else:
+    print("You need to grow some more to ride.")
