fdebff5bc8339cb1da7c0dc040a3b11183830fd6
naoboco <naomiebocobza@gmail.com>
2026-09-13 21:41:04 +0000

    e6

---
+++ DI_Bootcamp/week8/day6/Exercicexp/exercice6/npm-beginner/app.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk");
+
+console.log(chalk.blue("Hello World!"));
+console.log(chalk.green.bold("Node.js is working!"));
+console.log(chalk.yellow("Welcome to Chalk!"));
+console.log(chalk.red.bold("This is a colorful message!"));
